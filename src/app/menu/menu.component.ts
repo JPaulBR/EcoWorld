01161b6ab6163dd32330f4b2bb3c306f634fa7e7
Jean Paul Barrit <jpbr25@yahoo.com>
2020-04-04 00:06:28 -0600

    delete campaign

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'app-menu',
@@ -7,8 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
 
-  constructor() { }
+  constructor(private storage:Storage) { }
 
   ngOnInit() {}
+
 
 }
--- src/app/pagina-centros/pagina-centros.page.ts
+++ src/app/pagina-centros/pagina-centros.page.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { MenuController } from '@ionic/angular';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MenuController, ToastController } from '@ionic/angular';
 import { Storage } from '@ionic/storage';
 import { UsuarioService } from '../tablas/usuarios/usuario.service';
 import { CentrosService } from '../tablas/centros/centros.service';
@@ -25,12 +25,14 @@ export class PaginaCentrosPage implements OnInit {
     {lugar:"El Carmen, Cartago", horario:"Lunes a Jueves 9:30-15:00", material:"Plástico", img:"https://image.flaticon.com/icons/svg/967/967811.svg"}
   ]
 
-  constructor(private menuCtrl:MenuController, private storage: Storage, private apt: UsuarioService, private apt2: CentrosService) {}
+  constructor(private menuCtrl:MenuController, private storage: Storage, private apt: UsuarioService, 
+    private apt2: CentrosService,public toastCtrl: ToastController) {}
 
   ngOnInit() {
     this.hide=true;
     this.hide2=false;
     this.storage.get('email').then((val) => {
+      console.log("email: ",val);
       this.apt.getUserByEmail2(val).subscribe(dato =>{
         this.listUser = dato;
         this.listUser.forEach(element => {
@@ -53,6 +55,24 @@ export class PaginaCentrosPage implements OnInit {
 
   toggleMenu(){
     this.menuCtrl.toggle();
+  }
+
+  openWhatsApp(){
+    window.open('https://api.whatsapp.com/send?phone=${+50689441001}');
+  }
+
+  deleteCampaign(ide:string){
+    this.apt2.deleteCampaign(ide);
+    this.verSnackBar("Deleted successfully","dark");
+  }
+
+  async verSnackBar(msj,tColor){
+    let toast = await this.toastCtrl.create({
+      message: msj,
+      duration: 3000,
+      color: tColor
+    });
+    toast.present();
   }
 
 }
