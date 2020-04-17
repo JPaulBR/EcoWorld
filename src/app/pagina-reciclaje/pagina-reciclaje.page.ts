38f904efcfe71e859a73912095510c8bf86e2605
Jean Paul Barrit <jpbr25@yahoo.com>
2020-04-16 21:40:48 -0600

    Trazar rutas

--- src/app/pagina-reciclaje/pagina-reciclaje.page.ts
+++ src/app/pagina-reciclaje/pagina-reciclaje.page.ts
@@ -6,6 +6,7 @@ import { ScreenOrientation } from '@ionic-native/screen-orientation/ngx';
 import { CentrosService } from '../tablas/centros/centros.service';
 import { Storage } from '@ionic/storage';
 import { UsuarioService } from '../tablas/usuarios/usuario.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-pagina-reciclaje',
@@ -16,19 +17,17 @@ export class PaginaReciclajePage implements OnInit {
 
   mapa:mapboxgl.Map;
   pulsingDot:any;
+  txt: string="shfhdf";
 
   constructor(private menuCtrl: MenuController,private navCtrl: NavController,
     private screenOrientation: ScreenOrientation, private apt:CentrosService,
-    private apt2:UsuarioService,private storage: Storage) { 
+    private apt2:UsuarioService,private storage: Storage,private http:HttpClient) { 
       this.screenOrientation.lock(this.screenOrientation.ORIENTATIONS.LANDSCAPE);
       this.screenOrientation.lock(this.screenOrientation.ORIENTATIONS.PORTRAIT);
       this.screenOrientation.unlock();
   }
 
   ngOnInit() {
-    /*if (this.mapa!=undefined){
-      this.mapa.remove();
-    }*/
     (mapboxgl as any).accessToken = environment.mapboxKey;
     navigator.geolocation.getCurrentPosition(pos=>{
       var lat = pos.coords.latitude;
@@ -39,7 +38,9 @@ export class PaginaReciclajePage implements OnInit {
         center: [lng, lat], // starting position lng lat
         zoom: 13.5 // starting zoom
       });
-
+      var start = [lng,lat];
+      this.createOriginDestiny(start);
+      //this.createRoute(start);
       this.mapa.addControl(
         new mapboxgl.GeolocateControl({
         positionOptions: {
@@ -50,8 +51,8 @@ export class PaginaReciclajePage implements OnInit {
       );
       this.apt.getCampaigns().subscribe(res=>{
         res.forEach(element=>{
-          var msj = element.horario;
-          msj= msj+"\n"+"Phone: "+element.telefono;
+          var msj = '<strong>Schedule: <p>'+element.horario+'</p></strong>'+'<strong>Phone: '+
+          '<p>'+element.telefono+'</p></strong>';
           this.createMarker(element.long,element.lat,"https://image.flaticon.com/icons/svg/2371/2371819.svg",msj);
         })
       })
@@ -73,7 +74,7 @@ export class PaginaReciclajePage implements OnInit {
       im.style.width = "30px";
       im.style.height = "30px";
       el.appendChild(im);
-      var popup = new mapboxgl.Popup({ offset: 25 }).setText(msj);
+      var popup = new mapboxgl.Popup({ offset: 25 }).setHTML('<strong>I am here</strong>');
       new mapboxgl.Marker(el)
         .setLngLat([lng,lat])
         .setPopup(popup)
@@ -83,7 +84,7 @@ export class PaginaReciclajePage implements OnInit {
       im.src = img;
       im.style.width = "25px";
       im.style.height = "25px";
-      var popup = new mapboxgl.Popup({ offset: 25 }).setText(msj);
+      var popup = new mapboxgl.Popup({ offset: 25 }).setHTML(msj);
       new mapboxgl.Marker(im)
         .setLngLat([lng,lat])
         .setPopup(popup)
@@ -95,5 +96,74 @@ export class PaginaReciclajePage implements OnInit {
     this.menuCtrl.toggle();
   }
 
+  createRoute(start:any,end:any){
+    //var end = [-83.03111606215913,9.998421359889164];
+    var url = 'https://api.mapbox.com/directions/v5/mapbox/driving/' + start[0] + ',' + start[1] +
+    ';' + end[0] + ',' + end[1] + '?steps=true&geometries=geojson&access_token=' + environment.mapboxKey;
+    this.http.get(url).subscribe(res=>{
+      var data = res["routes"][0];
+      var route = data.geometry.coordinates;
+      console.log(route);
+      var geojson  = {
+        type: 'Feature' as const,
+        properties: {},
+        geometry: {
+          type: 'LineString' as const,
+          coordinates: route
+        }
+      };
+      if (this.mapa.getSource("route")){
+        var r = this.mapa.getSource('route') as mapboxgl.GeoJSONSource;
+        r.setData(geojson);
+      }
+      else{
+        this.mapa.addLayer({
+          id:"route",
+          type: "line",
+          source: {
+            type : "geojson",
+            data: {
+              type: "Feature",
+              properties: {},
+              geometry: {
+                type: "LineString",
+                coordinates: geojson.geometry.coordinates
+              }
+            } 
+          },
+          layout: {
+            "line-join": "round",
+            "line-cap": "round"
+          },
+          paint:{
+            "line-color": "#3887be",
+            "line-width": 5,
+            "line-opacity": 0.75
+          }
+        })
+      }
+    });
+  }
+
+  createOriginDestiny(start:any){
+    this.mapa.on("click",res=>{
+      var coordsObj = res.lngLat;
+      var coords = Object.keys(coordsObj).map(k=>{
+        return coordsObj[k]
+      });
+      var end = {
+        type: 'FeatureCollection',
+        features: [{
+          type: 'Feature',
+          properties: {},
+          geometry: {
+            type: 'Point',
+            coordinates: coords
+          }
+        }]
+      };
+      this.createRoute(start,coords);  
+    });
+  }
 
 }
